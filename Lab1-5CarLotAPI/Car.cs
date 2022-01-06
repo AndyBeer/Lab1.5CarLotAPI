71dd536fab679924a024edc8259eccb7bb17a29e
Andy Beer <AndrewBeer@RocketMortgage.com>
2022-01-06 08:38:03 -0500

    first commit

---
+++ Lab1-5CarLotAPI/Car.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab1_5CarLotAPI
+{
+    public class Car
+    {
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+        public Colors Color { get; set; }
+        public Car(string Make, string Model, int Year, Colors Color)
+        {
+            this.Make = Make;
+            this.Model = Model;
+            this.Year = Year;
+            this.Color = Color;
+        }
+
+    }
+    public enum Colors
+        {
+            black,
+            grey,
+            silver,
+            white,
+            yellow,
+            orange,
+            red,
+            green,
+            blue,
+            purple
+        }
+}
---
+++ Lab1-5CarLotAPI/Controllers/CarController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab1_5CarLotAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CarController : ControllerBase
+    {
+        public List<Car> carLot = new List<Car>
+        {
+            new Car("Ford", "Bronco", 1985, Colors.red),
+            new Car("Ford", "Escort", 1986, Colors.blue),
+            new Car("Chevrolet", "Caprice", 1983, Colors.purple),
+            new Car("Honda", "Accord", 1990, Colors.grey),
+            new Car("Cadillac", "El Dorado", 1985, Colors.black),
+            new Car("Lincoln", "Continental", 1985, Colors.red),
+            new Car("Ford", "F150", 1975, Colors.blue),
+            new Car("Honda", "Civic", 1985, Colors.white),
+            new Car("Toyota", "Tacoma", 1992, Colors.purple),
+            new Car("Toyota", "Tercel", 1992, Colors.silver),
+        };
+
+        [HttpGet]
+        public List<Car> GetAllCars()
+        {
+            return carLot;
+        }
+
+        [HttpGet("{index}")]
+        public Car GetSpecificCar(int index)
+        {
+            return carLot[index];
+        }
+
+        [HttpGet("SearchByColor/{color}")]
+        public List<Car> GetCarsByColor(string color)
+        {
+            List<Car> colorList = carLot.Where(x => x.Color.ToString().Contains(color.ToLower())).ToList();
+            return colorList;
+        }
+
+        [HttpGet("SearchByYear/{year}")]
+        public List<Car> GetCarsByYear(int year)
+        {
+            List<Car> yearList = carLot.Where(x => x.Year==year).ToList();
+            return yearList;
+        }
+
+        [HttpGet("SearchByMake/{make}")]
+        public List<Car> GetCarsByMake(string make)
+        {
+            List<Car> makeList = carLot.Where(x => x.Make.ToLower() == make.ToLower()).ToList();
+            return makeList;
+        }
+    }
+}
